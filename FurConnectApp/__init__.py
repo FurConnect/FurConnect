3af9823e0e42f312f16b64a7f95e220002ae6295
Tabitha <naylahanegan@gmail.com>
2026-06-17 13:02:25 -0400

    Update __init__.py

--- FurConnectApp/__init__.py
+++ FurConnectApp/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "1.2.0"  # Update this for each release; hybrid version is set dynamically in settings.py
+__version__ = "1.3.1"  # Update this for each release; hybrid version is set dynamically in settings.py
